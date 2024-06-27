51d085240964b1eb438871359876d3044ed02c6c
ARAFAT <munshiarafathussain@gmail.com>
2024-06-27 10:02:25 +0600

    JS basic khatam

---
+++ JavaScript/ChaiCode/05_Iteration/02_while.js
@@ -0,0 +1,22 @@
+// while loop
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+const myArray = ["ironman", "thor", "hulk", "captain", "blackwidow"];
+let j = 0;
+while (j < myArray.length) {
+  console.log(myArray[j]);
+  j++;
+}
+
+
+// do while loop
+let k = 11;
+do {
+  console.log(k); // must run at least once
+  k++;
+} while (k <= 5);
